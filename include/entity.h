dc5b97406f6067a6362bdf598a6f54d2163978f5
Joshua Dunn <89932329+JoshDunn88@users.noreply.github.com>
2024-11-01 20:17:34 -0400

    partial entity code

--- include/entity.h
+++ include/entity.h
@@ -13,39 +13,72 @@ typedef struct Entity_S
 	GFC_Vector3D	position;
 	GFC_Vector3D	rotation;
 	GFC_Vector3D	scale; 
-	Model			*model;
+	Model			*model; //entity's model if it has one
 
 	//behavior
 	void (*think) (struct Entity_S* self); //called every frame for entity to decide things
 	void (*update) (struct Entity_S* self); //called every frame to update its state
 	int (*draw) (struct Entity_S* self); //custom drawing code, if -1 skip
-	void (*free) (struct Entity_S* self); //called when entity is cleaned up to clean custom data
-	void			*data //entity custom data beyond basics
+	void (*free) (void* medata); //called when entity is cleaned up to clean custom data
+	void* data;                            //entity custom data beyond basics
 }Entity;
 
 /*
- * @brief allocated a blank entity for use
+ * @brief initializes entity management system and queues cleanup on exit
  * @param maxEnts how many entities can exist at the same time
 */
 void entity_system_init(Uint32 maxEnts);
 
+void entity_system_close();
+
 /*
- * @brief allocated a blank entity for use
- * @param maxEnts how many entities can exist at the same time
+ * @brief clean up active entities
+ * @param ignore do not clean this up, maxEnts how many entities can exist at the same time
 */
-void entity_system_init(Uint32 maxEnts);
+void entity_clear_all(Entity* ignore);
+
+
+
+
 /*
- * @brief allocated a blank entity for use
- * @return NULL on failure or a pointer to initialized entity
+ * @brief think for all entities
+ *  
+
+*/
+void entity_think_all();
+
+/*
+ * @brief update for all entities
+ *
+
+*/
+void entity_think_all();
+
+/*
+ * @brief draw all active entities
+ * @param 
+
+*/
+void entity_draw_all();
+
+/*
+ * @brief get a blank entity for use
+ * @return NULL on failure (no more room), or a pointer to initialized entity
 
 */
 Entity* entity_new();
 
 /*
- * @brief allocated a blank entity for use
- * @return NULL on failure or a pointer to initialized entity
+ * @brief clean up  entity and free its spot
+ * @param self the entity to free
 
 */
-Entity* entity_free();
+void entity_free(Entity* self);
+
+void entity_draw(Entity* self);
+
+void entity_think(Entity* self);
+
+void entity_update(Entity* self);
 
 #endif
--- src/entity.c
+++ src/entity.c
@@ -8,24 +8,117 @@ typedef struct
 	Uint32 entityMax;
 }EntityManager;
 
-static EntityManager entity_manager = { 0 };
+static EntityManager _entity_manager = { 0 }; //initialize local global entity manager
 
-/*
- * @brief allocated a blank entity for use
- * @param maxEnts how many entities can exist at the same time
-*/
+void entity_system_close() {
+
+	entity_clear_all(NULL);
+	if (_entity_manager.entityList) free(_entity_manager.entityList);
+	memset(&_entity_manager, 0, sizeof(EntityManager));
+}
+
 void entity_system_init(Uint32 maxEnts) 
 {
-	if (entity_manager.entityList) {
+	if (!maxEnts) 
+	{
+		slog("cannot allocate 0 entities");
+	}
+	if (_entity_manager.entityList) {
 		slog("entity manager already exists");
 		return;
 	}
-	entity_manager.entityList = gfc_allocate_array(sizeof(Entity), maxEnts);
-	if (!entity_manager.entityList) {
+	_entity_manager.entityList = gfc_allocate_array(sizeof(Entity), maxEnts);
+	if (!_entity_manager.entityList) {
 		slog("failed to allocate %i entities for the entity manager", maxEnts);
 	}
-	entity_manager.entityMax = maxEnts;
-	atexit(entity_system_close)
+	_entity_manager.entityMax = maxEnts;
+	atexit(entity_system_close);
+}
+
+
+
+void entity_clear_all(Entity* ignore) 
+{
+	int i;
+	for (int i = 0; i < _entity_manager.entityMax; i++) {
+		if (&_entity_manager.entityList[i] == ignore) continue;
+		if (!_entity_manager.entityList[i]._inuse) continue;
+		//do specific free
+		entity_free(&_entity_manager.entityList[i]);
+		//need to free the entity pointer itself somewhere too
+		//if (_entity_manager.entityList[i].free) _entity_manager.entityList[i].free(&_entity_manager.entityList[i]); //changed to pass self? is .data in vid
+	}
+}
+
+void entity_think_all() 
+{
+	int i;
+	for (int i = 0; i < _entity_manager.entityMax; i++) {
+		if (!_entity_manager.entityList[i]._inuse) continue;
+		entity_think(&_entity_manager.entityList[i]);
+	}
+}
+
+void entity_update_all()
+{
+	int i;
+	for (int i = 0; i < _entity_manager.entityMax; i++) {
+		if (!_entity_manager.entityList[i]._inuse) continue;
+		entity_update(&_entity_manager.entityList[i]);
+	}
+}
+
+void entity_draw_all()
+{
+	int i;
+	for (i = 0; i < _entity_manager.entityMax; i++) {
+		if (!_entity_manager.entityList[i]._inuse) continue;
+		//do draw
+		entity_draw(&_entity_manager.entityList[i]);
+	}
+
+}
+
+Entity* entity_new()
+{
+	int i;
+	for (i = 0; i < _entity_manager.entityMax; i++)
+	{
+		if (_entity_manager.entityList[i]._inuse) continue;//skip ones in use
+		memset(&_entity_manager.entityList[i], 0, sizeof(Entity));
+		_entity_manager.entityList[i]._inuse = 1;
+		_entity_manager.entityList[i].scale = gfc_vector3d(1, 1, 1);
+		return &_entity_manager.entityList[i];
+	}
+	slog("no more open entity slots");
+	return NULL; //no more entity slots
+
+}
+
+void entity_free(Entity* self)
+{
+	if (!self) return;
+	gf3d_model_free(self->model);
+
+	//free anthing special that may have been allocated FOR this
+	if (self->free) self->free(self->data);
+	//free self itself??
+}
+
+void entity_think (Entity* self)
+{
+	if (!self) return;
+	//basic think stuff here before self think
+	//methink
+	if (self->think) self->think(self);
+}
+
+void entity_update(Entity* self)
+{
+	if (!self) return;
+	//basic think stuff here before self update
+	//meupdate
+	if (self->update) self->update(self);
 }
 
 void entity_draw(Entity *self)
@@ -33,8 +126,12 @@ void entity_draw(Entity *self)
 	GFC_Matrix4 matrix;
 	if (!self) return;
 
-	if (self->draw(self) == -1 return;
-
+	//this is weird, old code commented out
+	//if (self->draw(self) == -1) return;
+	if (self->draw) {
+		self->draw(self);
+		return;
+	}
 	gfc_matrix4_from_vectors(
 		matrix,
 		self->position,
@@ -51,43 +148,7 @@ void entity_draw(Entity *self)
 
 
 }
-void entity_draw_all() 
-{
-	int i;
-	for (i = 0; i < entity_manager.entityMax; i++) {
-		if (!entity_manager.entityList[i]._inuse) continue;
-	}
 
-}
 
-Entity* entity_new()
-{
-	int i;
-	for (i = 0; i < entity_manager.entityMax; i++)
-	{
-		if (entity_manager.entityList[i]._inuse) continue;//skip ones in use
-		memset(&entity_manager.entityList[i], 0, sizeof(Entity));
-		entity_manager.entityList[i]._inuse = 1;
-		entity_manager.entityList[i].scale = gfc_vector3d(1,1,1);
-		return &entity_manager.entityList[i];
-	}
-	return NULL; //no more entity slots
 
-}
 
-void entity_free(Entity *self)
-{
-	if (!self) return;
-	//free anthing that may have been allocated FOR this
-
-	if (self->free) self->free(self);
-	gf3d_model_free(self->model);
-}
-void entity_system_clode() {
-	int i;
-	for (int i = 0; i < entity_manager.entityMax; i++) {
-		if (!entity_manager.entityList[i]._inuse) continue;
-
-	}
-
-}
